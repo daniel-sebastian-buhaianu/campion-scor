37d83848c483a1283b3476df9b0da160179e6385
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-11-12 11:57:24 +0200

    Update scor.cpp - scor 10

--- scor.cpp
+++ scor.cpp
@@ -1,163 +1,129 @@
 #include <fstream>
 #include <cstring>
-#define MAXCIF 101
+#define LGMAX 102
 using namespace std;
 ifstream fin("scor.in");
 ofstream fout("scor.out");
-struct NrMare
-{
-	int nrcif;
-	char cif[MAXCIF];
-};
-void citesteNrMare(NrMare &);
-int comparaNrMari(NrMare, NrMare);
-void scadeNrMari(NrMare, NrMare, NrMare &);
-void imparteNrMareLaNrMic(NrMare, int, NrMare &);
-void adunaNrMari(NrMare, NrMare, NrMare &);
+struct NrMare { int nrCifre, cifre[LGMAX]; };
+void citesteNrMare(NrMare&);
 void afiseazaNrMare(NrMare);
+int ultimaCifra(NrMare);
+int comparaNrMari(NrMare, NrMare);
+void scadeNrMari(NrMare, NrMare, NrMare&);
+void imparteNrMareLaNrMic(NrMare, int, NrMare&);
+void adunaNrMari(NrMare, NrMare, NrMare&);
 int main()
 {
-	int T, i, ucs, ucd;
-	NrMare s, d, sum, dif, a, b;
-	fin >> T;
-	for (i = 0; i < T; i++)
-	{
-		citesteNrMare(s);
-		citesteNrMare(d);
-		if (comparaNrMari(s, d) < 0)
-		{
-			fout << "impossible\n";
-		}
-		else
-		{
-			adunaNrMari(s, d, sum);
-			scadeNrMari(s, d, dif);
-			ucs = (int)sum.cif[0];
-			ucd = (int)dif.cif[0];
-			if (ucs % 2 || ucd % 2)
-			{
-				fout << "impossible\n";
-			}
-			else
-			{
-				imparteNrMareLaNrMic(sum, 2, a);
-				imparteNrMareLaNrMic(dif, 2, b);
-				afiseazaNrMare(a);
-				fout << ' ';
-				afiseazaNrMare(b);
-				fout << '\n';
-			}
-		}
-	}
-	fin.close();
-	fout.close();
-	return 0;
+    int n;
+    NrMare s, d, aux, b, a;
+    fin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        citesteNrMare(s);
+        citesteNrMare(d);
+        if (ultimaCifra(s)%2 || comparaNrMari(s, d) < 0)
+            fout << "impossible";
+        else
+        {
+            scadeNrMari(s, d, aux);
+            imparteNrMareLaNrMic(aux, 2, b);
+            adunaNrMari(b, d, a);
+            afiseazaNrMare(a);
+            fout << ' ';
+            afiseazaNrMare(b);
+        }
+        fout << '\n';
+    }
+    fin.close();
+    fout.close();
+    return 0;
 }
-void adunaNrMari(NrMare a, NrMare b, NrMare & s)
+void adunaNrMari(NrMare a, NrMare b, NrMare& c)
 {
-	int i, t, val;
-	if (a.nrcif < b.nrcif)
-	{
-		for (i = a.nrcif; i < b.nrcif; i++)
-		{
-			a.cif[i] = 0;
-		}
-		s.nrcif = b.nrcif;
-	}
-	else
-	{
-		for (i = b.nrcif; i < a.nrcif; i++)
-		{
-			b.cif[i] = 0;
-		}
-		s.nrcif = a.nrcif;
-	}
-	for (t = i = 0; i < s.nrcif; i++)
-	{
-		val = a.cif[i] + b.cif[i] + t;
-		s.cif[i] = val%10;
-		t = val/10;
-	}
-	if (t)
-	{
-		s.cif[s.nrcif++] = t;
-	}
+    int i, t, val;
+    c.nrCifre = a.nrCifre > b.nrCifre ? a.nrCifre : b.nrCifre;
+    if (a.nrCifre < b.nrCifre)
+        for (i = a.nrCifre; i < b.nrCifre; i++)
+            a.cifre[i] = 0;
+    else if (b.nrCifre < a.nrCifre)
+        for (i = b.nrCifre; i < a.nrCifre; i++)
+            b.cifre[i] = 0;
+    for (t = i = 0; i < c.nrCifre; i++)
+    {
+        val = a.cifre[i]+b.cifre[i]+t;
+        c.cifre[i] = val%10;
+        t = val/10;
+    }
+    if (t)
+        c.cifre[i] = t, c.nrCifre++;
 }
-void imparteNrMareLaNrMic(NrMare a, int b, NrMare & nr)
+void imparteNrMareLaNrMic(NrMare a, int b, NrMare& c)
 {
-	int r;
-	if (a.nrcif == 1 && a.cif[0] == 0)
-	{
-		r = nr.cif[0] = 0;
-		nr.nrcif = 1;
-		return;
-	}
-	r = 0;
-	for (int i = a.nrcif-1; i >= 0; i--)
-	{
-		int val = r*10 + a.cif[i];
-		nr.cif[i] = val/b;
-		r = val%b;
-	}
-	for (nr.nrcif = a.nrcif;
-	     nr.cif[nr.nrcif-1] == 0 && nr.nrcif > 1;
-	     nr.nrcif--);
+    if (ultimaCifra(a) == 0 && a.nrCifre == 1)
+        c.cifre[0] = 0, c.nrCifre = 1;
+    else
+    {
+        int r, i, val;
+        for (r = 0, i = a.nrCifre-1; i >= 0; i--)
+        {
+            val = r*10 + a.cifre[i];
+            c.cifre[i] = val/b;
+            r = val%b;
+        }
+        for (i = a.nrCifre-1; c.cifre[i] == 0 && i > 0; i--);
+        c.nrCifre = i+1;
+    }
 }
-void scadeNrMari(NrMare a, NrMare b, NrMare & d)
+void scadeNrMari(NrMare a, NrMare b, NrMare& c)
 {
-	int i, t, val;
-	for (i = b.nrcif; i < a.nrcif; i++)
-	{
-		b.cif[i] = 0;
-	}
-	for (t = i = 0; i < a.nrcif; i++)
-	{
-		val = a.cif[i] - b.cif[i] + t;
-		if (val < 0)
-		{
-			d.cif[i] = val + 10;
-			t = -1;
-		}
-		else
-		{
-			d.cif[i] = val;
-			t = 0;
-		}
-	}
-	for (d.nrcif = a.nrcif;
-	     d.cif[d.nrcif-1] == 0 && d.nrcif > 1;
-	     d.nrcif--);
+    int i, t, val;
+    for (i = 0; i < LGMAX; i++)
+        c.cifre[i] = 0;
+    for (i = b.nrCifre; i < a.nrCifre; i++)
+        b.cifre[i] = 0;
+    for (t = i = 0; i < a.nrCifre; i++)
+    {
+        val = a.cifre[i]-b.cifre[i]-t;
+        if (val < 0)
+            val += 10, t=1;
+        else
+            t = 0;
+        c.cifre[i] = val;
+    }
+    for (i = a.nrCifre-1; c.cifre[i] == 0 && i > 0; i--);
+    c.nrCifre = i+1;
 }
 int comparaNrMari(NrMare a, NrMare b)
 {
-	if (a.nrcif > b.nrcif) return 1;
-	if (a.nrcif < b.nrcif) return -1;
-	int i;
-	for (i = 0; i < a.nrcif && a.cif[i] == b.cif[i]; i++);
-	if (i == a.nrcif) return 0;
-	if (a.cif[i] > b.cif[i]) return 1;
-	return -1;
+    if (a.nrCifre < b.nrCifre)
+        return -1;
+    if (a.nrCifre > b.nrCifre)
+        return 1;
+    int i;
+    for (i = a.nrCifre-1; a.cifre[i] == b.cifre[i] && i >= 0; i--);
+    if (i < 0)
+        return 0;
+    if (a.cifre[i] < b.cifre[i])
+        return -1;
+    return 1;
 }
-void afiseazaNrMare(NrMare nr)
+int ultimaCifra(NrMare a)
 {
-	for (int i = nr.nrcif-1; i >= 0; i--)
-	{
-		fout << (int)nr.cif[i];
-	}
+    return a.cifre[0];
 }
-void citesteNrMare(NrMare & nr)
+void afiseazaNrMare(NrMare a)
 {
-	int lg, i;
-	char s[MAXCIF+1];
-	fin >> s;
-	nr.nrcif = lg = strlen(s);
-	for (i = lg-1; i >= 0; i--)
-	{
-		nr.cif[lg-i-1] = s[i]-'0';
-	}
-	for (i = lg; i < MAXCIF; i++)
-	{
-		nr.cif[i] = 0;
-	}
+    for (int i = a.nrCifre-1; i >=0; i--)
+        fout << a.cifre[i];
 }
-// scor 10/100 (nu inteleg de ce, toate testele imi ies)
+void citesteNrMare(NrMare& a)
+{
+    char s[LGMAX];
+    fin >> s;
+    a.nrCifre = strlen(s);
+    int i;
+    for (i = 0; i < a.nrCifre; i++)
+        a.cifre[i] = s[a.nrCifre-i-1]-'0';
+    for (i = a.nrCifre; i < LGMAX; i++)
+        a.cifre[i] = 0;
+}
